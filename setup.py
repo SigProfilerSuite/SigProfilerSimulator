267df6d7b3aaef550ff206ea251f688b6f353d39
josecorcab <jcordoba@cnio.es>
2026-05-06 22:36:49 +0200

    Revert "Merge pull request #13 from AlexandrovLab/seeds_support"
    This reverts commit 596edd02d50fc37dd13505da99486a1abfd14a46, reversing
    changes made to 1e8bb243d24abf20817ca8e87e64d2577a71b019.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
         return long_description
 
 
-VERSION = "1.2.2"
+VERSION = "1.2.1"
 
 
 def write_version_py(filename="SigProfilerSimulator/version.py"):
@@ -24,7 +24,7 @@ def write_version_py(filename="SigProfilerSimulator/version.py"):
 # THIS FILE IS GENERATED FROM SIGPROFILERSIMULATOR SETUP.PY
 short_version = '%(version)s'
 version = '%(version)s'
-Update = 'v1.2.2: Add seed support and reproducibility tests'
+Update = 'v1.2.1: Add support for mm39'
 	
 	"""
     fh = open(filename, "w")
